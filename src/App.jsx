d6d8771c697f810c23bdd2251ac4e603c90de627
Yuto299 <ota.se.ke@outlook.jp>
2025-08-25 00:03:16 +0900

    アロー関数にした

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import "./App.css";
 import emojipedia from "./emojipedia";
 import Card from "./Card";
 
-function createCard(char) {
-  return (
-    <Card
-      key={char.id}
-      emoji={char.emoji}
-      name={char.name}
-      meaning={char.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <>
       <h1>
         <span>emojipedia</span>
       </h1>
-      {emojipedia.map(createCard)}
+      {emojipedia.map((char) => (
+        <Card
+          key={char.id}
+          emoji={char.emoji}
+          name={char.name}
+          meaning={char.meaning}
+        />
+      ))}
     </>
   );
 }
